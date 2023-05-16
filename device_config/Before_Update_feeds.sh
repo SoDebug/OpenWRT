f2087b6c2ec53d3d4d223c59e091b29d73347394
SoDebug <30922923+SoDebug@users.noreply.github.com>
2023-05-17 00:14:19 +0800

    Before_Update_feeds.sh: 自定义UA，而不是FFF...

--- device_config/Before_Update_feeds.sh
+++ device_config/Before_Update_feeds.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-git clone https://github.com/Zxilly/UA2F package/UA2F
+git clone https://github.com/SoDebug/UA2F package/UA2F
